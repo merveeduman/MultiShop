e10137e613122fe4eb1c54ae1d89e6d4f112bbad
Merve Duman <139700200+merveeduman@users.noreply.github.com>
2026-04-13 16:15:27 +0300

    add MediatR

---
+++ Services/Order/Core/MultiShop.Order.Application/Interfaces/IOrderingRepository.cs
@@ -0,0 +1,9 @@
+﻿using MultiShop.Order.Domain.Entities;
+
+namespace MultiShop.Order.Application.Interfaces
+{
+    public interface IOrderingRepository
+    {
+        List<Ordering> GetOrderingsByUserId(string id);
+    }
+}
---
+++ Services/Order/Core/MultiShop.Order.Application/Services/ServiceRegistration.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MultiShop.Order.Application.Services
+{
+    public static class ServiceRegistration
+    {
+        public static void AddApplicationService(this IServiceCollection services)
+        {
+            services.AddMediatR(cfg =>
+                cfg.RegisterServicesFromAssembly(typeof(ServiceRegistration).Assembly));
+        }
+    }
+}
